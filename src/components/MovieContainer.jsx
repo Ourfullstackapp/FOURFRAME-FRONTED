ef1c3eb74e4b8bad91b0cd733abe5b9289f93403
Owen <owenkaris9512@gmail.com>
2025-06-27 02:01:25 +0300

    style.css

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import MovieCard from './MovieCard';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import MovieDetail from './components/MovieDetail';
+import Profile from './components/Profile'; 
 
-const MovieContainer = ({ movies }) => (
-  <>
-    {movies.map((movie) => (
-      <MovieCard key={movie.id} movie={movie} />
-    ))}
-  </>
-);
+const App = () => {
+  const token = localStorage.getItem('token');
 
-export default MovieContainer;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+        <Route path="/profile" element={token ? <Profile /> : <Navigate to="/login" />} /> 
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
