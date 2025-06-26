064835e7f9b463ef227f32743b90f0c0a9117228
Tyra <tyra.mwai@student.moringaschool.com>
2025-06-27 02:14:00 +0300

    Updated components

--- src/components/MovieContainer.jsx
+++ src/components/MovieContainer.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import MovieDetail from './components/MovieDetail';
-import Profile from './components/Profile'; 
+import MovieCard from './MovieCard';
 
-const App = () => {
-  const token = localStorage.getItem('token');
+const MovieContainer = ({ movies }) => (
+  <>
+    {movies.map((movie) => (
+      <MovieCard key={movie.id} movie={movie} />
+    ))}
+  </>
+);
 
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/movies/:id" element={<MovieDetail />} />
-        <Route path="/profile" element={token ? <Profile /> : <Navigate to="/login" />} /> 
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
+export default MovieContainer;
